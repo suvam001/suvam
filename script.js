f15d4d8009b1bf9f7d3a48745808643b7dbfad47
Suvam <16056604+suvam001@users.noreply.github.com>
2023-08-24 18:08:40 +0530

    script.js

--- script.js
+++ script.js
@@ -1,50 +1,24 @@
-function typeText(element, text, callback) {
+function typeContent(target, content, callback) {
     let i = 0;
+    const elements = content.match(/<[^>]*>[^<]*<\/[^>]*>|<[^/>]*\/>/g) || []; // Split by HTML tags
     const timer = setInterval(() => {
-        if (i < text.length) {
-            element.textContent += text.charAt(i);
+        if (i < elements.length) {
+            target.innerHTML += elements[i];
             i++;
         } else {
             clearInterval(timer);
             callback();
         }
-    }, 30);
-}
-
-function typeContent(contentWrapper, callback) {
-    const textNodes = [];
-    function collectTextNodes(node) {
-        if (node.nodeType === Node.TEXT_NODE && node.nodeValue.trim() !== '') {
-            textNodes.push(node);
-        } else {
-            for (let child of node.childNodes) {
-                collectTextNodes(child);
-            }
-        }
-    }
-    collectTextNodes(contentWrapper);
-
-    let i = 0;
-    function typeNextNode() {
-        if (i < textNodes.length) {
-            const textNode = textNodes[i];
-            const text = textNode.nodeValue;
-            textNode.nodeValue = '';
-            typeText(textNode, text, typeNextNode);
-            i++;
-        } else {
-            callback();
-        }
-    }
-    typeNextNode();
+    }, 100); // Adjust the typing speed here
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    const contentWrapper = document.querySelector('.content-wrapper');
-    contentWrapper.style.opacity = 0; // Initially hidden (fully transparent)
+    const navWrapper = document.querySelector('.nav-wrapper');
+    const originalContent = navWrapper.innerHTML;
 
-    setTimeout(() => {
-        contentWrapper.style.opacity = 1; // Fade in
-        typeContent(contentWrapper, () => {});
-    }, 500); // Delay before starting the typing effect
+    // Clear the original content
+    navWrapper.innerHTML = '';
+
+    // Start typing the original content
+    typeContent(navWrapper, originalContent, () => {});
 });
