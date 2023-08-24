530aa8b89563ccefac88ed996d48e0e63746eefb
Suvam <16056604+suvam001@users.noreply.github.com>
2023-08-24 12:54:19 +0530

    script.js

--- script.js
+++ script.js
@@ -1,110 +1,23 @@
-body {
-    font-family: Arial, sans-serif;
-    margin: 0;
-    padding: 0;
-    cursor: none;
-    background: black;
-    color: silver;
-}
+// Smooth Scrolling
+$('.nav-button').on('click', function (event) {
+    event.preventDefault();
+    var target = $(this.getAttribute('href'));
+    $('html, body').stop().animate({
+        scrollTop: target.offset().top
+    }, 1000);
+});
 
-.custom-cursor {
-    width: 15px;
-    height: 15px;
-    position: absolute;
-    transform: translate(-50%, -50%);
-    pointer-events: none;
-    z-index: 10000;
-}
+// Custom Cursor
+$(document).mousemove(function (e) {
+    $('#custom-cursor').css({
+        left: e.pageX,
+        top: e.pageY
+    });
+});
 
-.custom-cursor:before,
-.custom-cursor:after {
-    content: '';
-    position: absolute;
-    background-color: #333;
-    width: 2px;
-    height: 100%;
-}
-
-.custom-cursor:before {
-    transform: rotate(45deg);
-}
-
-.custom-cursor:after {
-    transform: rotate(-45deg);
-}
-
-header {
-    background: #f0f0f0;
-    padding: 20px;
-    text-align: center;
-}
-
-header h1,
-header p,
-h2 {
-    color: goldenrod;
-}
-
-nav {
-    background: #f0f0f0;
-    padding: 10px;
-    text-align: center;
-    overflow: auto;
-    white-space: nowrap;
-}
-
-.nav-button {
-    padding: 10px;
-    text-decoration: none;
-    color: #333;
-    transition: background 0.3s;
-    display: inline-block;
-}
-
-.nav-button:hover {
-    background: #f0f0f0;
-}
-
-
-.skill-container {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin-bottom: 10px;
-}
-
-.progress-bar-label {
-    flex: 1; /* Take up remaining space */
-    text-align: left; /* Align text to the left */
-    margin-right: 10px; /* Space between label and progress bar */
-}
-
-.progress-bar {
-    flex: 3; /* Take up 3 times the space as the label */
-    position: relative;
-    width: 2px; /* Set the width to 50% of the parent container, or use a fixed value like 200px */
-    height: 12px;
-    background-color: #f0f0f0;
-    border-radius: 6px;
-    box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
-    overflow: hidden;
-}
-
-.progress-bar-fill {
-    position: absolute;
-    left: 0;
-    top: 0;
-    height: 100%;
-    background-color: yellow; 
-    width: 0;
-}
-
-
-
-/* Responsive design for mobile devices */
-@media screen and (max-width: 768px) {
-    nav .nav-button {
-        display: block;
-        text-align: center;
-    }
-}
+// Progress Bars for Skills
+$('.progress-bar').each(function () {
+    var value = $(this).data('value');
+    $(this).append('<div class="progress-bar-fill"></div>');
+    $(this).find('.progress-bar-fill').animate({ width: value + '%' }, 1000);
+});
